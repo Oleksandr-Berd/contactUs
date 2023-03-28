0653a498016332be8ab2207b1a59cb47beddbc5c
Oleksandr-Berd <alex.berd86@gmail.com>
2023-03-28 12:49:40 +0300

    mobile home is done

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,10 +1,12 @@
 import ContactForm from "../../Components/Contact/ContactForm/ContactForm";
 import ContactInformation from "../../Components/Contact/ContactInformation/ContactInformation";
 import ContactTitle from "../../Components/Contact/ContactTitle/ContactTitle";
+import Header from "../../Components/Header/Header";
 
 const ContactPage = () => {
   return (
     <>
+      <Header />
       <ContactTitle />
       <ContactInformation />
       <ContactForm />
